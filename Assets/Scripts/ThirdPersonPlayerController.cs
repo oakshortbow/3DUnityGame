9aa366f898e42c7c4a1b2bd6f7e145c8e114f3b1
oakshortbow <sigmatotal1@gmail.com>
2020-05-08 08:44:17 -0400

    Dashing Tuning

--- Assets/Scripts/ThirdPersonPlayerController.cs
+++ Assets/Scripts/ThirdPersonPlayerController.cs
@@ -21,6 +21,9 @@ public class ThirdPersonPlayerController : MonoBehaviour
 
     private List<GameObject> clones = new List<GameObject>();
     private int cloneCount = 0;
+    private int renderClone = 0;
+
+    private const int MAX_CLONES = 25;
 
     // Start is called before the first frame update
     private void Start()
@@ -75,17 +78,13 @@ public class ThirdPersonPlayerController : MonoBehaviour
     private void Blink() {
         this.GetComponent<BoxCollider>().enabled = false;
         this.GetComponent<Rigidbody>().useGravity = false;
-
-
-
         this.transform.localScale = Vector3.zero;
-
         rb.DOMove(transform.position + transform.forward * dodgeDistance, dodgeTime).SetUpdate(UpdateType.Fixed, true).OnUpdate(ClonePlayer).OnComplete(FinishBlink);
     }
 
 
     private void ClonePlayer() {
-        if(clones.Count < 25) {
+        if(clones.Count < 3) {
             GameObject clone = Instantiate(gameObject, transform.position, transform.rotation);
             clones.Add(clone);
             clone.transform.localScale = new Vector3(1, 1, 1);
@@ -93,8 +92,13 @@ public class ThirdPersonPlayerController : MonoBehaviour
             Destroy(clone.GetComponent<Animator>());
             Destroy(clone.GetComponent<BoxCollider>());
             Destroy(clone.GetComponent<Rigidbody>());
-            Destroy(clone.GetComponent<AnimationController>());
-            return;
+            Destroy(clone.GetComponent<AnimationController>());  
+            SkinnedMeshRenderer[] skinMeshList = clone.GetComponentsInChildren<SkinnedMeshRenderer>();
+            foreach (SkinnedMeshRenderer smr in skinMeshList)
+            {
+                smr.material.DOFloat(2, "_AlphaThreshold", 5f);
+            }
+            return;       
         }
         int cloneIndex = cloneCount++;
         clones[cloneIndex].transform.position = this.transform.position;
@@ -107,11 +111,6 @@ public class ThirdPersonPlayerController : MonoBehaviour
         this.transform.localScale = new Vector3(1, 1, 1);
         this.GetComponent<BoxCollider>().enabled = true;
         this.GetComponent<Rigidbody>().useGravity = true;
-        foreach(GameObject clone in clones) {
-            clone.SetActive(false);
-            //clones.Remove(clone);
-        }
-        Debug.Log(clones.Count);
         cloneCount = 0;
     }
 }
